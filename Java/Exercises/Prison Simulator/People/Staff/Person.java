76c9cb3c927e5699e6e70570320d9f0eef5d48ab
zekster131 <159156975+zekster131@users.noreply.github.com>
2025-01-21 16:09:01 +0000

    Create Person.java

---
+++ Java/Exercises/Prison Simulator/People/Staff/Person.java
@@ -0,0 +1,15 @@
+package People.Staff;
+
+public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
