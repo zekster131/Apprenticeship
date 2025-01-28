1865ed442286814e8af26e62d4bb06d16746aa9e
zekster131 <159156975+zekster131@users.noreply.github.com>
2025-01-28 13:12:20 +0000

    Update Password Strength Checker.java

--- Java/Exercises/Password Strength Checker.java
+++ Java/Exercises/Password Strength Checker.java
@@ -3,25 +3,6 @@
 # Logic that checks a pasword and returns a rating (very weak, weak, medium, strong, very strong) - check against common passwords and if it matches it should be weak
 # Object to call a method that checks the password (object will pass through the password)
 
-# class Students:
-#   def __init__(self, first, last, age):
-#     self.first = first
-#     self.last = last
-#     self.age = age
-#     self.full = first + ' ' + last
-
-# student1 = Students('John', 'Doe', 20)
-# student2 = Students('Jane', 'Doe', 21)
-
-# print(student1.age)
-# print(student2.first)
-# print(student2.full)
-
-# Make a class
-
-# Logic that checks a pasword and returns a rating (very weak, weak, medium, strong, very strong) - check against common passwords and if it matches it should be weak
-# Object to call a method that checks the password (object will pass through the password)
-
 class PasswordChecker:
   def __init__(self):
     self.common_passwords = ["password", "123456", "qwerty", "admin", "password123"]
